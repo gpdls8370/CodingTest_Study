5795f0e9ac2f6320c2b87b2adb9eebb11b3e011f
박혜인 <46310524+gpdls8370@users.noreply.github.com>
2023-09-14 21:46:55 +0900

    [Gold IV] Title: 좋다, Time: 12 ms, Memory: 2156 KB -BaekjoonHub

---
+++ 백준/Gold/1253. 좋다/좋다.cc
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	int N;
+	cin >> N;
+	vector<long> nums(N, 0);
+
+	for (int i = 0; i < N; i++) {
+		cin >> nums[i];
+	}
+
+	sort(nums.begin(), nums.end());
+	int cnt = 0;
+
+	for (int i = 0; i < N; i++) {
+		long temp = nums[i];
+		int start = 0;
+		int end = N - 1;
+
+		while (start < end) {
+			int sum = nums[start] + nums[end];
+
+			if (sum > temp) {
+				end--;
+			}
+			else if (sum < temp) {
+				start++;
+			}
+			else {
+				if (start == i) {
+					start++;
+				}
+				else if (end == i) {
+					end--;
+				}
+				else {
+					cnt++;
+					break;
+				}
+			}
+		}
+	}
+
+	cout << cnt;
+}
