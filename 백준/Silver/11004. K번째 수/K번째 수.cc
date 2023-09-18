ad269ae6567fc0f675113b34a2c2d2dca9a8abc6
박혜인 <46310524+gpdls8370@users.noreply.github.com>
2023-09-18 17:40:08 +0900

    [Silver V] Title: K번째 수, Time: 1048 ms, Memory: 21552 KB -BaekjoonHub

---
+++ 백준/Silver/11004. K번째 수/K번째 수.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main(){
+
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	int N , K;
+	cin >> N >> K;
+
+	vector<int> arr(N);
+
+	for(int i=0;i<N;i++){
+		cin >> arr[i];
+	}
+
+	sort(arr.begin() , arr.end());
+
+	cout << arr[K - 1];
+}
