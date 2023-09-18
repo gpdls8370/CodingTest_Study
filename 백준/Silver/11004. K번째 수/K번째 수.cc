2b8b64c31c986539814392f8d5d6bdcf7b1d596d
박혜인 <46310524+gpdls8370@users.noreply.github.com>
2023-09-18 19:26:01 +0900

    [Silver V] Title: K번째 수, Time: 712 ms, Memory: 21552 KB -BaekjoonHub

--- 백준/Silver/11004. K번째 수/K번째 수.cc
+++ 백준/Silver/11004. K번째 수/K번째 수.cc
@@ -3,6 +3,9 @@
 #include <algorithm>
 using namespace std;
 
+void quickSort(vector<int>& arr , int start , int end , int K);
+int partition(vector<int>& arr , int start , int end);
+
 int main(){
 
 	ios::sync_with_stdio(0);
@@ -18,7 +21,43 @@ int main(){
 		cin >> arr[i];
 	}
 
-	sort(arr.begin() , arr.end());
+	quickSort(arr , 0 , N - 1 , K - 1);
 
 	cout << arr[K - 1];
+}
+
+void quickSort(vector<int> &arr, int start, int end, int K){
+	int pivot = partition(arr , start , end);
+
+	if (pivot == K) return;
+	else if (pivot > K) quickSort(arr , start , pivot - 1 , K);
+	else quickSort(arr , pivot + 1 , end , K);
+}
+
+int partition(vector<int> &arr, int start, int end){
+
+	if(start + 1 ==end){
+		if (arr[start] > arr[end]) swap(arr[start] , arr[end]);
+		return end;
+	}
+
+	int middle = (start + end) / 2;
+
+	swap(arr[start] , arr[middle]);
+
+	int pivot = arr[start];
+
+	int left = start + 1;
+	int right = end;
+
+	while(left <= right){
+		while (arr[right] > pivot && right > 0) right--;
+		while (arr[left] < pivot && left < arr.size() - 1) left++;
+
+		if (left <= right) swap(arr[left++] , arr[right--]);
+	}
+
+	swap(arr[start] , arr[right]);
+
+	return right;
 }
